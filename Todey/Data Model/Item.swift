7d91be93492cbe6ddc3d56158fc18143949e0bc2
Michael L'Hommedieu <michael.lhommedieu@michaels-macbook-air.local>
2018-09-11 09:42:35 +0800

    Convert to MVC design pattern

---
+++ Todey/Data Model/Item.swift
@@ -0,0 +1,16 @@
+//
+//  Item.swift
+//  Todey
+//
+//  Created by Michael L'Hommedieu on 9/9/18.
+//  Copyright © 2018 Michael L'Hommedieu. All rights reserved.
+//
+
+import Foundation
+
+class Item{
+    
+    var title: String = ""
+    var done: Bool = false
+    
+}
